53321fd028459577de5c54f978e55624b0cdfd4c
alexpatsias <92375934+alexpatsias@users.noreply.github.com>
2022-04-17 18:48:17 +0300

    Update main.js

--- main.js
+++ main.js
@@ -18,38 +18,6 @@ window.addEventListener("DOMContentLoaded", init);
 let map;
 let sidebar;
 let panelID = "my-info-panel";
-
-////
-
-(() => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success, error);
-  } else {
-    alert("Geolocation is not supported by this browser");
-  }    
-})();
-
-function success(position) {
-  const latitude = position.coords.latitude;
-  const longitude = position.coords.longitude;
-  getMap(latitude, longitude);
-}
-
-function error() {
-  alert("Unable to retrieve location");
-}
-
-function getMap(latitude, longitude) {
-  const map = L.map("map").setView([latitude, longitude], 7);
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
-  L.marker([latitude, longitude], {
-	  draggable:true,
-	  title: "Text for marker",
-  }).addTo(map);
-
-	
-	/////
-	
 	
 /*
  * init() is called when the page has loaded
