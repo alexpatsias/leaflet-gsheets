891cf53359aa192961bce660740b789f6cf65c90
alexpatsias <92375934+alexpatsias@users.noreply.github.com>
2022-04-19 13:22:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -287,33 +287,5 @@ function parseGeom(gj) {
       type = "MultiPolygon";
     }
     return [{ type: "Feature", geometry: { type: type, coordinates: gj } }];
-  }
-if (!navigator.geolocation) {
-  console.log("Your browser doesn't support geolocation feature!");
-} else {
-  navigator.geolocation.getCurrentPosition(getPosition);
-}
-	
-var marker, circle, lat, long, accuracy;
-
-function getPosition(position) {
-  // console.log(position)
-  lat = position.coords.latitude;
-  long = position.coords.longitude;
-  accuracy = position.coords.accuracy;
-
-  if (marker) {
-    map.removeLayer(marker);
-  }
-
-  if (circle) {
-    map.removeLayer(circle);
-  }
-
-  marker = L.marker([lat, long]);
-  circle = L.circle([lat, long], { radius: accuracy });
-
-  var featureGroup = L.featureGroup([marker, circle]).addTo(map);
-
-  map.fitBounds(featureGroup.getBounds()); 
+  }	
 }
