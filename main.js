e5cac26434e1058380a20925dc8aa720e802a500
alexpatsias <92375934+alexpatsias@users.noreply.github.com>
2022-04-19 12:51:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -54,23 +54,23 @@ if (!navigator.geolocation) {
 var marker, circle, lat, long, accuracy;
 
 function getPosition(position) {
-  // console.log(position)
+  console.log(position)
   lat = position.coords.latitude;
   long = position.coords.longitude;
-  //accuracy = position.coords.accuracy;
+  accuracy = position.coords.accuracy;
 
   if (marker) {
     map.removeLayer(marker);
   }
 
-  //if (circle) {
-    //map.removeLayer(circle);
-  //}
+  if (circle) {
+    map.removeLayer(circle);
+  }
 
   marker = L.marker([lat, long]);
-  //circle = L.circle([lat, long], { radius: accuracy });
+  circle = L.circle([lat, long], { radius: accuracy });
 
-  var featureGroup = L.featureGroup(marker).addTo(map);
+  var featureGroup = L.featureGroup([marker,circle]).addTo(map);
 
   map.fitBounds(featureGroup.getBounds());
 
