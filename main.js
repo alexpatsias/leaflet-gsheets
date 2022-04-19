2bc77701256111703d1296d20a5455033ba4ece1
alexpatsias <92375934+alexpatsias@users.noreply.github.com>
2022-04-19 12:50:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ function getPosition(position) {
 
   var featureGroup = L.featureGroup(marker).addTo(map);
 
-  //map.fitBounds(featureGroup.getBounds());
+  map.fitBounds(featureGroup.getBounds());
 
   console.log(
     "Your coordinate is: Lat: " +
